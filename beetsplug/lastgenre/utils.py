64f07b089abdf9f74610920348fbf4c0367953d9
J0J0 Todos <2733783+JOJ0@users.noreply.github.com>
2026-06-24 08:58:27 +0200

    Update beetsplug/lastgenre/utils.py
    Co-authored-by: Šarūnas Nejus <snejus@protonmail.com>

--- beetsplug/lastgenre/utils.py
+++ beetsplug/lastgenre/utils.py
@@ -75,6 +75,6 @@ def normalize_genre(
                 )
                 continue
             if expanded != genre:
-                logger.extra_debug("aliased: {} -> {}", genre, expanded)
+                logger.extra_debug("replaced using 'aliases': {} -> {}", genre, expanded)
             return expanded
     return genre_lower
