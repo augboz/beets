4ca632dbbfea0b945d3227a4ae3f1424fa5c3827
Šarūnas Nejus <snejus@protonmail.com>
2026-06-27 17:14:21 +0100

    Rename query_from_strings -> build_and_query

--- beets/dbcore/__init__.py
+++ beets/dbcore/__init__.py
@@ -16,6 +16,8 @@
 Library.
 """
 
+from beets.util.deprecation import deprecate_for_maintainers, deprecate_imports
+
 from .db import Database, Index, Model, Results
 from .query import (
     AndQuery,
@@ -25,12 +27,19 @@ from .query import (
     OrQuery,
     Query,
 )
-from .queryparse import (
-    parse_sorted_query,
-    query_from_strings,
-    sort_from_strings,
-)
+from .queryparse import build_and_query, parse_sorted_query, sort_from_strings
 from .types import Type
+
+
+def __getattr__(name: str):
+    if name == "query_from_strings":
+        deprecate_for_maintainers(
+            f"'beets.dbcore.{name}'", "'beets.dbcore.build_and_query'"
+        )
+        return build_and_query
+
+    return deprecate_imports(__name__, {}, name)
+
 
 __all__ = [
     "AndQuery",
@@ -44,7 +53,7 @@ __all__ = [
     "Query",
     "Results",
     "Type",
+    "build_and_query",
     "parse_sorted_query",
-    "query_from_strings",
     "sort_from_strings",
 ]
